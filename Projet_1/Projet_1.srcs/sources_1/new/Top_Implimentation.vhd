36a62a6688aae0887dd141a1161479b2918300c4
JamesFullum <james.fullum.1@ens.etsmtl.ca>
2024-02-18 19:15:15 -0500

    Succesful Synthesis and Place and Route

--- Projet_1/Projet_1.srcs/sources_1/new/Top_Implimentation.vhd
+++ Projet_1/Projet_1.srcs/sources_1/new/Top_Implimentation.vhd
@@ -6,8 +6,7 @@
 -- Auteur   : Guillaume et James
 -- Création : 2024-01-28
 --------------------------------------------------------------------------------
--- Description : Machine à État Fini qui permet de contrôler le comportement de 
---               feu de traffiques
+-- Description : TOP pour implementation sur la carte
 --------------------------------------------------------------------------------
 
 library IEEE;
@@ -23,26 +22,33 @@ entity TOP is
 
   -- Déclaration des ports d'entrées et sorties du MÉF
   port (
-    i_clk    : in  std_logic;
-    i_bi     : in  std_logic;
-    i_bap    : in  std_logic;  -- Bouton à piétons
-    o_sa     : out std_logic;
-    o_fp_v   : out std_logic;  -- Feu vert pour le FP
-    o_fp_j   : out std_logic;  -- Feu jaune pour le FP
-    o_fp_r   : out std_logic;  -- Feu rouge pour le FP
-    o_fp_f   : out std_logic;
-    o_fs_v   : out std_logic;  -- Feu vert pour le FS
-    o_fs_j   : out std_logic;  -- Feu jaune pour le FS
-    o_fs_r   : out std_logic;  -- Feu rouge pour le FS
-    o_fs_f   : out std_logic;  
-    o_fptp   : out std_logic;   -- Feu pour le FPTP
-    o_fptp_f : out std_logic
+    clk  : in std_logic;
+    btnC : in std_logic;
+    btnU : in std_logic;
+    led  : out std_logic_vector(8 downto 0)
   );
+  
+    -- Définition des ports du module testé en signaux
+  signal clk_int    : std_logic;
+  signal bi_int     : std_logic;  
+  signal bap_int    : std_logic;
+  signal sa_int     : std_logic;
+  signal fp_v_int   : std_logic;
+  signal fp_j_int   : std_logic;
+  signal fp_r_int   : std_logic;
+  signal fp_f_int   : std_logic;
+  signal fs_v_int   : std_logic;
+  signal fs_j_int   : std_logic;
+  signal fs_r_int   : std_logic;
+  signal fs_f_int   : std_logic;
+  signal fptp_int   : std_logic;   
+  signal fptp_f_int : std_logic;
 end TOP;
 
 architecture rtl of TOP is
 
-  component MEF is
+
+  component MEF_Projet_1 is
       -- Déclaration des génériques
       generic (
         G_DELAI       : positive;
@@ -73,32 +79,73 @@ architecture rtl of TOP is
   component ila_0
      port(
         clk : in std_logic;
-        probe_0 : in std_logic;
-        probe_1 : in std_logic;
-        probe_2 : in std_logic;
-        probe_3 : in std_logic;
-        probe_4 : in std_logic;
-        probe_5 : in std_logic;
-        probe_6 : in std_logic;
-        probe_7 : in std_logic;
-        probe_8 : in std_logic;
-        probe_9 : in std_logic;
-        probe_10 : in std_logic;
-        probe_11 : in std_logic;
-        probe_12 : in std_logic;
-        probe_13 : in std_logic;
-        probe_14 : in std_logic;
-        probe_15 : in std_logic;
-        probe_16 : in std_logic      
+        probe0 : in std_logic;
+        probe1 : in std_logic;
+        probe2 : in std_logic;
+        probe3 : in std_logic;
+        probe4 : in std_logic;
+        probe5 : in std_logic;
+        probe6 : in std_logic;
+        probe7 : in std_logic;
+        probe8 : in std_logic;
+        probe9 : in std_logic;
+        probe10 : in std_logic;
+        probe11 : in std_logic;
+        probe12 : in std_logic      
      );
   end component;
   
-  
- 
 begin
+
+ clk_int <= clk;
+ bi_int  <= btnC;
+ bap_int <= btnU;
+ led(0)  <= fp_v_int;
+ led(1)  <= fp_j_int;
+ led(2)  <= fp_r_int;
+ led(3)  <= fp_v_int;
+ led(4)  <= fp_j_int;
+ led(5)  <= fp_r_int;
+ led(6)  <= fptp_int;
+ led(7)  <= sa_int;
+ 
+ DUT : MEF_Projet_1
+    generic map (
+      G_DELAI       => G_DELAI,
+      G_DELAI_JAUNE => G_DELAI_JAUNE,
+      G_DELAI_SIZE  => G_DELAI_SIZE)
+    port map (
+      i_clk      => clk,
+      i_bi       => bi_int,
+      i_bap      => bap_int,
+      o_sa       => sa_int,
+      o_fp_v     => fp_v_int,
+      o_fp_j     => fp_j_int,
+      o_fp_r     => fp_r_int,
+      o_fp_f     => fp_f_int,
+      o_fs_v     => fs_v_int,
+      o_fs_j     => fs_j_int,
+      o_fs_r     => fs_r_int,
+      o_fs_f     => fs_f_int,
+      o_fptp     => fptp_int,
+      o_fptp_f   => fptp_f_int);   
    
-   
-   
-   
+     ILA : ila_0
+     port map(
+        clk      => clk_int,
+        probe0  => bi_int,
+        probe1  => bap_int,
+        probe2  => sa_int,
+        probe3  => fp_v_int,
+        probe4  => fp_j_int,
+        probe5  => fp_r_int,
+        probe6  => fp_f_int,
+        probe7  => fs_v_int,
+        probe8  => fs_j_int,
+        probe9  => fs_r_int,
+        probe10 => fs_f_int,
+        probe11 => fptp_int,
+        probe12 => fptp_f_int    
+        );
    
 end rtl;
